0917bae794f1f5f7ab46292f03d59ad5dde17d26
Diego Bran <diegobran@umes.edu.gt>
2026-05-20 15:17:32 -0600

    Implement PWA Core capabilities

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['vitra_logo.png', 'pwa-192x192.png', 'pwa-512x512.png'],
+      manifest: {
+        name: 'Vitra Holo App - Holograms as a Service',
+        short_name: 'Vitra Holo',
+        description: 'Plataforma para proyectar modelos 3D mediante hardware holográfico para PYMES',
+        theme_color: '#09090b',
+        background_color: '#09090b',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ]
 })
